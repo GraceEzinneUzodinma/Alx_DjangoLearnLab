1dfff8123f5ae8fa86a2fd261fe616cc7d4a5df8
grace uzodinma <guzodinma2@gmail.com>
2025-11-05 00:00:38 +0100

    error

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,5 +1,6 @@
 from relationship_app.models import Author, Book, Library, Librarian
-Book.objects.filter(author__name ='')
+Author.objects.get(name=author_name)
+objects.filter(author=author)
 Book.library.all()
 Library.objects.get(name=library_name)
 books.all()
