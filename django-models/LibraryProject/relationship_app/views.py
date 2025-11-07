d13159594ad34541a33d913fc2818a42876d092b
grace uzodinma <guzodinma2@gmail.com>
2025-11-07 22:06:53 +0100

    django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,7 @@ from django.views.generic.detail import DetailView
 from .models import Library
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
 def list_books(request):
       books = Book.objects.all()  
       context = {'book_list': books}  
@@ -13,3 +14,6 @@ class LibraryDetailView(DetailView):
   model = Book
   template_name = 'relationship_app/library_detail.html'
   context_object_name = 'books'
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'relationship_app/register.html'
