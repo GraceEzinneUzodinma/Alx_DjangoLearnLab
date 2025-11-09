0bd228d8e8044c1eea868a3a90512b99ef76378a
grace uzodinma <guzodinma2@gmail.com>
2025-11-09 15:52:17 +0100

    trial

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -7,6 +7,12 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    class Meta:
+        permissions= [
+             ("can_add_book", "can_add_book"),
+             ("can_change_book", "can_change_book"),
+             ("can_delete_book", "can_delete_book"),
+        ]
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book)
