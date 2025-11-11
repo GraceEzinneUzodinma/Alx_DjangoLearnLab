0ae188997535b228d81fc8b10bb2f1a88c504b33
grace uzodinma <guzodinma2@gmail.com>
2025-11-11 15:37:09 +0100

    config

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,10 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, date_of_birth, profile_photo):
+        user = self.model(
+            date_of_birth= date_of_birth,
+            profile_photo= profile_photo
+        )
+        user.save(using = self._db)
+        return user
+
+    def create_superuser(self, date_of_birth, profile_photo):
+        user = self.create_user(
+            date_of_birth= date_of_birth,
+            profile_photo= profile_photo
+        )
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        return user
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField
     profile_photo = models.ImageField
+    is_staff = models.BooleanField(default=False)
+    is_superuser =models.BooleanField(default=False)
+    objects = CustomUserManager()
 # Create your models here.
