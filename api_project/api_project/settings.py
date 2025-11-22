79a78c69d57aa85654cd6b6233d514754a6daa42
grace uzodinma <guzodinma2@gmail.com>
2025-11-22 16:24:35 +0100

    api

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = 'static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAunthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
 
