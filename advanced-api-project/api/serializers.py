af1fa11680e3538961883d3c8f1719139acb4083
grace uzodinma <guzodinma2@gmail.com>
2025-11-27 18:39:17 +0100

    advanced api

---
+++ advanced-api-project/api/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Book, Author
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
+        
+    def validate(self, date):
+            current_year = date.today().year
+            if date > current_year:
+                raise serializers.ValidationError("Publication year cannot be in the future")
+            return date
+    
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = 'name'
