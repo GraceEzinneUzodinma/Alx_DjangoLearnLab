664572021ef275aa28736044ac4f226bef80f52f
grace uzodinma <guzodinma2@gmail.com>
2025-12-03 22:30:57 +0100

    blog

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views  import generic
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+
+
+class UserRegisterView(generic.CreateView):
+    form_class = UserCreationForm
+    template = 'blog/register.html'
+
+
 
 # Create your views here.
