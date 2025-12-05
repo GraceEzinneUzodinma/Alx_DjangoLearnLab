0e0d7ff1b9ebd14bf8486b160e0978424c2532a0
grace uzodinma <guzodinma2@gmail.com>
2025-12-05 08:39:44 +0100

    blog

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -4,6 +4,12 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
 from django import forms
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework import generics, viewsets
+from django_filters import rest_framework as filters
+from rest_framework import filters
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -41,6 +47,36 @@ class ProfileUpdateView(LoginRequiredMixin, View):
             return redirect("profile")
 
         return render(request, "profile.html", {"form": form})
+    
+class BlogListView(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'author', 'publication_year']
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['title', 'publication_year']
+
+
+class BlogDetailView(generics.RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+class BlogCreateView(generics.CreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+
+class BlogUpdateView(generics.UpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+
+class BlogDeleteView(generics.DestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
 
         
 
