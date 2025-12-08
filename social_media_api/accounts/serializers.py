b5715270ebe6052d7729b5d027e3b4f67e430aa4
grace uzodinma <guzodinma2@gmail.com>
2025-12-08 20:05:02 +0100

    accounts

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -4,7 +4,7 @@ from django.contrib.auth import authenticate
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
-User = get_user_model()
+User = get_user_model().objects.create_user()
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
