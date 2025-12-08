e6224fabb2d6749b5834e9aa886069b6d1f8fb00
grace uzodinma <guzodinma2@gmail.com>
2025-12-08 20:06:15 +0100

    url

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 
 urlpatterns=[
-    path('Register/', RegisterView.as_view(), name = 'accounts-register' ),
-    path('Login/',LoginView.as_view(), name= 'accounts-login'),
+    path('register/', RegisterView.as_view(), name = 'accounts-register' ),
+    path('login/',LoginView.as_view(), name= 'accounts-login'),
     path('Token/', TokenView.as_view(), name= 'accounts-token')
 ]
