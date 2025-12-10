67bb690d2b071a25dbe336f36a8c3f4fefe10a8d
grace uzodinma <guzodinma2@gmail.com>
2025-12-10 22:22:27 +0100

    posts

---
+++ social_media_api/posts/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from accounts.models import CustomUser
+
+class Post(models.Model):
+    title = models.CharField(max_length= 200)
+    content = models.TextField
+    created_at = models.DateTimeField
+    updated_at = models.DateTimeField
+    author = models.ForeignKey(CustomUser, on_delete= models.CASCADE)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete= models.CASCADE)
+    author = models.ForeignKey(CustomUser, on_delete= models.CASCADE)
+    content = models.TextField
+    created_at = models.DateTimeField
+    updated_at = models.DateTimeField
+
+
+# Create your models here.
---
+++ social_media_api/posts/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from .serializers import PostSerializer, CommentSerializer
+from rest_framework import generics
+from .models import Post, Comment
+
+
+
+class PostListView(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetailView(generics.RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    
+
+class PostCreateView(generics.CreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostUpdateView(generics.UpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDeleteView(generics.DestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    
+
+
+# Create your views here.
