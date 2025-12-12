4c8ec3b1485d9473c41a70a145ce5962669a9143
grace uzodinma <guzodinma2@gmail.com>
2025-12-12 21:32:06 +0100

    url

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,9 +1,11 @@
-from .views import RegisterView, LoginView, TokenView
+from .views import RegisterView, LoginView, TokenView, FollowUserView, UnfollowUserView
 from django.urls import path
 
 
 urlpatterns=[
     path('register/', RegisterView.as_view(), name = 'accounts-register' ),
     path('login/',LoginView.as_view(), name= 'accounts-login'),
-    path('Token/', TokenView.as_view(), name= 'accounts-token')
+    path('Token/', TokenView.as_view(), name= 'accounts-token'),
+    path('/follow/<int:user_id>/', FollowUserView.as_view(), name= 'follow'),
+    path('/unfollow/<int:user_id>/', UnfollowUserView.as_view, name= 'unfollow')
 ]
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .serializers import PostSerializer, CommentSerializer
-from rest_framework import viewsets
+from rest_framework import viewsets, generics, permissions
 from .models import Post, Comment
 
 
@@ -12,4 +12,13 @@ class PostViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+
+class FeedView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get_queryset(self):
+        following = self.request.user.following.all()
+        return Post.objects.filter(author__in=following).order_by('-created_at')
+
 # Create your views here.
